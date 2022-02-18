1999d64c072361d18fc87eb8ea3914d68a38c621
Neil Ryan L <neilryanlipaod@gmail.com>
2022-02-18 10:48:11 +0800

    Add custom decorator for imageUrl validation
    Used class-validator to create a custom decorator. Imported dependencies
    such as file-type to determine if it's an image mime type. Got was also
    imported to fetch the stream from the URL.

---
+++ src/common/input-validation/is-image-url.decorator.ts
@@ -0,0 +1,32 @@
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+import got from 'got';
+import { fileTypeFromStream } from 'file-type';
+
+@ValidatorConstraint({ async: true })
+export class IsImageUrlConstraint implements ValidatorConstraintInterface {
+  async validate(imageUrl: string, args: ValidationArguments) {
+    const stream = got.stream(imageUrl);
+    const fileType = await fileTypeFromStream(stream);
+    const isImage = fileType.mime.substring(0, 5) === 'image';
+    return isImage;
+  }
+}
+
+export function IsImageUrl(validationOptions: ValidationOptions) {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: IsImageUrlConstraint,
+    });
+  };
+}
