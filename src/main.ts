f325702e6ec925628f90517a4016cacddb9b4658
Neil Ryan L <neilryanlipaod@gmail.com>
2022-02-18 10:29:02 +0800

    Add helmet for security

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
     }),
   );
+
   await app.listen(3000);
 }
 bootstrap();
